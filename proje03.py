273d2cd47f485c359c3c5110ea81946058562264
ebrargunay <99511513+ebrargunay@users.noreply.github.com>
2022-12-30 21:36:22 +0300

    Add files via upload

--- proje03.py
+++ proje03.py
@@ -1,29 +1,36 @@
+import tkinter as tk
 from gensim.models import KeyedVectors
+pencere = tk.Tk()
+pencere.title("Benzer Kelimeler")
+
+etiket = tk.Label(pencere, text="Aramak İstediğiniz Anahtar Kelimeyi Giriniz:")
+etiket.pack(padx=20, pady=20)
+
+metinAlani = tk.Entry(pencere)
+metinAlani.pack(padx=20, pady=20)
+
+buton = tk.Button(pencere, text="Aramayı Başlatmak İçin Tıklayın")
+buton.pack(padx=20, pady=20)
+
 print("Bu Modelde Wikipedia Makalelerindeki Kelimelerin Benzerlik Oranlarını Ölçüyoruz Denemek İçin Lütfen Bekleyiniz.")
 print("Model Yükleniyor...")
 kelimeVektoru = KeyedVectors.load_word2vec_format('trModel100', binary=True)
 print(kelimeVektoru)
 
+def benzerKelimeler(self):
+    anahtarKelimeler = metinAlani.get().lower()
+    listeKelimeler = anahtarKelimeler.split()
 
-def benzerKelimeler():
-    anahtarKelime = input("Aramak İstediğiniz Anahtar Kelimeyi Giriniz: ").lower()
-    print(" ")
-    print("Girilen Kelime : " + str(anahtarKelime))
-    print(" ")
-    print("Anahtar Kelimeye En Yakın Kelimeler:")
-    print(" ")
-    oneriler = (kelimeVektoru.most_similar(positive=anahtarKelime))
-    print(oneriler)
-    print(" ")
-    print("Ziyaret Etmek İsteyebileceğiniz Linkler:")
-    print(" ")
+    for anahtarKelime in listeKelimeler:
+        print("Girilen Kelime: " + str(anahtarKelime))
+        oneriler = (kelimeVektoru.most_similar(positive=anahtarKelime))
+        print(oneriler)
 
-    # İlk 10 öneriyi göster
-    for index, oneri in enumerate(oneriler):
-        if anahtarKelime not in oneri[0]:
-            print("https://www.google.com/search?q=" + oneri[0])
-
-    benzerKelimeler()
+        for oneri in oneriler:
+            if anahtarKelime not in oneri[0]:
+                print("https://www.google.com.tr/search?q=" + oneri[0])
 
 
-benzerKelimeler()
+buton.bind("<Button-1>", benzerKelimeler)
+
+pencere.mainloop()
